164cab60d66fc2439fcc932004f77b6d6526dc6b
Vladimir-Kod <kodvv@yandex.ru>
2022-12-25 15:21:26 +0500

    Completed task

--- Task/Beginner Series #2 Clock/task-27.js
+++
@@ -1,8 +0,0 @@
-function past(h, m, s){
-    //#Happy Coding! ^_^
-    var hours = h * 3600000;
-    var minuts = m * 60000;
-    var seconds = s * 1000;
-    var result = hours + minuts + seconds;
-    return result; 
-};
