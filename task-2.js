458e2100e9b4995b416e4354b35489ab52b6cae7
Vladimir-Kod <kodvv@yandex.ru>
2022-12-25 17:05:41 +0500

    Comleted task

--- task-2.js
+++ task-2.js
@@ -6,12 +6,6 @@ function evenOrOdd(number) {
 // "Четное" для четных чисел или "Нечетное" для нечетных чисел.
 
 
-function evenOrOdd(number) {
-    if(number % 2 === 0) {     
-    return "Even";
-    } else {
-    return "Odd";
-    } 
-  };
+
 
 // оператор (argument % 2 === 0), если четное число вернет true, если нечетное, вернет false
